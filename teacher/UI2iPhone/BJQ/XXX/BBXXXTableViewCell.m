15425725cee177991ac74f8c401854e95db72090
qing0602 <zq62@vip.qq.com>
2014-06-14 14:57:58 +0800

    fix bug

--- teacher/UI2iPhone/BJQ/XXX/BBXXXTableViewCell.m
+++ teacher/UI2iPhone/BJQ/XXX/BBXXXTableViewCell.m
@@ -126,7 +126,7 @@
     if (imageUrl == [NSNull null] || [imageUrl isEqualToString:@""] ) {
         contentPreView.hidden = NO;
         thumbnail.hidden = YES;
-        contentPreView.text = data.content;
+        if (data.content != [NSNull null]) contentPreView.text = data.content;
     }else
     {
         thumbnail.hidden = NO;
