da312945a5698d0c86ea723670965739f923b514
qing0602 <zq62@vip.qq.com>
2014-07-22 14:36:23 +0800

    add cpDAONotifymsg

---
+++ teacher/Core/DB/DAO/CPDAONotifyMessage.h
@@ -0,0 +1,13 @@
+//
+//  CPDAONotifyMessage.h
+//  teacher
+//
+//  Created by ZhangQing on 14-7-22.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "BaseDAO.h"
+
+@interface CPDAONotifyMessage : BaseDAO
+
+@end
---
+++ teacher/Core/DB/DAO/CPDAONotifyMessage.m
@@ -0,0 +1,13 @@
+//
+//  CPDAONotifyMessage.m
+//  teacher
+//
+//  Created by ZhangQing on 14-7-22.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "CPDAONotifyMessage.h"
+
+@implementation CPDAONotifyMessage
+
+@end
