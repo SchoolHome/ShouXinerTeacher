b14c6b7ed3f559a9c675f24ad9031ca3c8e9974b
wangshuo <9708022@qq.com>
2014-09-26 00:50:12 +0800

    video

---
+++ teacher/UI2iPhone/Video/CropVideo.h
@@ -0,0 +1,13 @@
+//
+//  CropVideo.h
+//  teacher
+//
+//  Created by singlew on 14-9-26.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CropVideo : NSObject
+
+@end
---
+++ teacher/UI2iPhone/Video/CropVideo.m
@@ -0,0 +1,13 @@
+//
+//  CropVideo.m
+//  teacher
+//
+//  Created by singlew on 14-9-26.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "CropVideo.h"
+
+@implementation CropVideo
+
+@end
