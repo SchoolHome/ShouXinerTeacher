51243700de2e096115aa62a60e05cce55469eab1
qing0602 <zq62@vip.qq.com>
2014-09-27 16:28:35 +0800

    add classes

---
+++ teacher/UI2iPhone/BJQ/WSP/BBWSPViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBWSPViewController.h
+//  teacher
+//
+//  Created by ZhangQing on 14-9-27.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "PalmViewController.h"
+
+@interface BBWSPViewController : PalmViewController
+
+@end
---
+++ teacher/UI2iPhone/BJQ/WSP/VideoConfirmViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoConfirmViewController.h
+//  teacher
+//
+//  Created by ZhangQing on 14-9-27.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "PalmViewController.h"
+
+@interface VideoConfirmViewController : PalmViewController
+
+@end
