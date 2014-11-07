e45c63c2f341992130d2d965e1e50a975b90eb73
mac <xin.song@idtechinfo.com>
2014-11-07 10:00:02 +0800

    faxianshouye

---
+++ teacher/UI2iPhone/FX/BBFXViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBFXViewController.h
+//  teacher
+//
+//  Created by mac on 14/11/7.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "PalmViewController.h"
+
+@interface BBFXViewController : PalmViewController
+
+@end
---
+++ teacher/UI2iPhone/FX/BBFXViewController.m
@@ -0,0 +1,50 @@
+//
+//  BBFXViewController.m
+//  teacher
+//
+//  Created by mac on 14/11/7.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "BBFXViewController.h"
+
+@interface BBFXViewController ()
+
+@end
+
+@implementation BBFXViewController
+
+-(id)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)loadView
+{
+    [super loadView];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
