76091803851bdcf5f7bd7155f4a7e60cbffa7a32
qing0602 <zq62@vip.qq.com>
2014-11-20 15:49:02 +0800

    add file

---
+++ teacher/UI2iPhone/ZJZ/Contacts/BBServiceAccountDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBServiceAccountDetailViewController.h
+//  teacher
+//
+//  Created by ZhangQing on 14-11-20.
+//  Copyright (c) 2014年 ws. All rights reserved.
+//
+
+#import "PalmViewController.h"
+
+@interface BBServiceAccountDetailViewController : PalmViewController
+
+@end
