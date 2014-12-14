d46136dd58eff3e9f400a59ad6062b50f1e0d8b3
qing0602 <zq62@vip.qq.com>
2014-12-15 01:29:31 +0800

    fix bug

--- teacher/AppDelegate.m
+++ teacher/AppDelegate.m
@@ -84,13 +84,21 @@
         image = [UIImage imageNamed:@"navbar_back"];
     }else{
         image = [UIImage imageNamed:@"navBarBg44"];
+
     }
-    [[UINavigationBar appearance] setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
-    [[UINavigationBar appearanceWhenContainedIn:[CustomNavigationController class], nil] setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
+//    [[UINavigationBar appearance] setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
+        [[UINavigationBar appearanceWhenContainedIn:[CustomNavigationController class], nil] setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
     
-    NSDictionary *attributes=[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:251/255.f green:76/255.f blue:7/255.f alpha:1.f],UITextAttributeTextColor,[UIFont boldSystemFontOfSize:18],UITextAttributeFont, nil];
-    //[[UINavigationBar appearance] setTitleTextAttributes:attributes];
-    [[UINavigationBar appearanceWhenContainedIn:[CustomNavigationController class], nil] setTitleTextAttributes:attributes];
+    if (IOS7) {
+        NSDictionary *attributes=[NSDictionary dictionaryWithObjectsAndKeys:
+                                  [UIColor colorWithRed:251/255.f green:76/255.f blue:7/255.f alpha:1.f],UITextAttributeTextColor,
+                                  [UIColor colorWithRed:251/255.f green:76/255.f blue:7/255.f alpha:1.f],NSForegroundColorAttributeName,
+                                  [UIFont boldSystemFontOfSize:18],UITextAttributeFont,
+                                  [NSValue valueWithUIOffset:UIOffsetZero],UITextAttributeTextShadowOffset, nil];
+        //[[UINavigationBar appearance] setTitleTextAttributes:attributes];
+        [[UINavigationBar appearanceWhenContainedIn:[CustomNavigationController class], nil] setTitleTextAttributes:attributes];
+    }
+
     
     
     [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
