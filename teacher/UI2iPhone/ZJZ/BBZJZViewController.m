c5aecc70f3346d9c8e0f5c6a2a0c8d1bb689da42
qing0602 <zq62@vip.qq.com>
2014-12-22 23:50:31 +0800

    fix confilt

--- teacher/UI2iPhone/ZJZ/BBZJZViewController.m
+++ teacher/UI2iPhone/ZJZ/BBZJZViewController.m
@@ -308,13 +308,7 @@
     NSMutableArray *tempTeachersArray = [[NSMutableArray alloc] init];
     NSMutableArray *tempParentsArray = [[NSMutableArray alloc] init];
     for (CPUIModelUserInfo *model in [CPUIModelManagement sharedInstance].friendArray) {
-        if ([model.lifeStatus integerValue] == 100022478) {
-<<<<<<< HEAD
-            NSLog(@"100022478");
-=======
-            NSLog(@"1");
->>>>>>> 795e7d3e8cea232afce755f359cc581e4350803d
-        }
+
         ContactsModel *tempModel = [[ContactsModel alloc] init];
         tempModel.modelID = [model.lifeStatus integerValue];
         tempModel.avatarPath = model.headerPath;
